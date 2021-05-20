0c129994e4d3e77ad17b535e80a91816bd1a12c8
srinupotnuru <iamsrinu3@gmail.com>
2021-05-20 21:23:02 +0530

    back-end

---
+++ static/app.js
@@ -0,0 +1,33 @@
+function copyToClipboard(text) {  
+    window.prompt("Copy to clipboard: Ctrl+C, Enter", text);
+  }
+function doit()
+{
+    var data={
+        'title':"",
+        'author':"",
+        'text':"",
+        'public':""};
+    var title=document.getElementById('title').value;
+    var author=document.getElementById('author');
+    var text=document.getElementById('paste');
+    var public=document.getElementById('private_check').checked;
+    
+    data['title']=title;
+    data['author']=author.value;
+    data['text']=text.value;
+    data['public']=public;
+
+    fetch("/create",{
+        method:"POST",
+        body:JSON.stringify({data}),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+          },
+    }).then((response) => response.json()).then((data)=>{
+        let url=window.location.href+data["result"];
+        document.getElementById('output').innerHTML=url;
+        copyToClipboard(url);
+    });
+    
+}
